76a41eb8db1da486274dd5975df66013a105774a
Lasculiviu <lasculiviu131@gmail.com>
2024-07-06 08:15:44 +0200

    ex1

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,31 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
+}
+
+
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const body = document.body;
+
+let colorInterval = null;
+
+
+function startColorChange() {
+    startButton.disabled = true;
+    stopButton.disabled = false;
+
+    colorInterval = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+
+function stopColorChange() {
+    startButton.disabled = false;
+    stopButton.disabled = true;
+
+    clearInterval(colorInterval);
+}
+
+startButton.addEventListener('click', startColorChange);
+stopButton.addEventListener('click', stopColorChange);
